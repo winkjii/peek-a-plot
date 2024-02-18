db44f7bcde930ecf7fbdb83f3306f77a0f69414d
theeramukt <chayanit9336@gmail.com>
2024-02-18 13:29:18 +0700

    import firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFsGPjGUSU92dlxV5q-4yKNX1-y9QLD7o",
+  authDomain: "peekaplot-887d5.firebaseapp.com",
+  projectId: "peekaplot-887d5",
+  storageBucket: "peekaplot-887d5.appspot.com",
+  messagingSenderId: "662587444067",
+  appId: "1:662587444067:web:1f7ebf77a76b467f742156",
+  measurementId: "G-TB7MVRBC29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
